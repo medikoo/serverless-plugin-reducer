19c5d71f3e5b8bdef46c7ef34679e19969ae77af
Mariusz Nowak <medyk@medikoo.com>
2017-06-16 17:46:13 +0200

    💥

---
+++ README.md
@@ -0,0 +1,24 @@
+# serverless-plugin-lambda-reducer
+## Reduce lambda package to host only lambda dependencies
+## Plugin for Serverless 0.5.x
+
+By default Serverless 0.5.x packages whole contents of folder that covers scope of lambda function.
+
+It raises issues in projects where we maintain many functions and want to share some modules between them.
+In such case scope folder of each lambda function covers not only shared modules but all other lambdas, and whole that content is being packaged for each lambda.
+
+This plugin ensures that aside of obligatory meta files, it's only dependencies of given lambda that are packaged and published to AWS
+
+### Installation
+
+	$ npm install serverless-plugin-lambda-reducer
+
+Activate plugin in `s-project.json`
+
+```json
+{
+  "plugins": [
+    "serverless-plugin-lambda-reducer"
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var last            = require("es5-ext/array/#/last")
+  , deferred        = require("deferred")
+  , path            = require("path")
+  , copy            = require("fs2/copy")
+  , cjsResolve      = require("cjs-module/resolve")
+  , getDependencies = require("cjs-module/get-dependencies");
+
+var resolve = path.resolve, dirname = path.dirname;
+
+module.exports = function (Serverless) {
+	Serverless.classes.Runtime.prototype.copyFunction = function (func, pathDist, stage, region) {
+		// 1. Copy initial logic from original:
+
+		// Status
+		Serverless.utils.sDebug(
+			stage + " - " + region + " - " + func.getName() + ": Copying in dist dir " + pathDist
+		);
+
+		// Extract the root of the lambda package from the handler property
+		var handlerFullPath = func
+			.getRootPath(last.call(func.handler.split("/")))
+			.replace(/\\/g, "/");
+
+		// Check handler is correct
+		if (!handlerFullPath.endsWith(func.handler)) {
+			return Promise.reject(
+				new Error(
+					"This function's handler is invalid and not in the file system: " + func.handler
+				)
+			);
+		}
+
+		var packageRoot = handlerFullPath.replace(func.handler, "");
+
+		// 2. Custom copy handling
+		var filter = this._processExcludePatterns(func, pathDist, stage, region);
+
+		var funcMetaPath = dirname(func._filePath);
+		return deferred(
+			// Meta: s-function.json
+			copy(func._filePath, resolve(pathDist, func._filePath.slice(packageRoot.length)), {
+				intermediate: true
+			}),
+			// Meta: event.json
+			copy(
+				resolve(funcMetaPath, "event.json"),
+				resolve(pathDist, funcMetaPath.slice(packageRoot.length), "event.json"),
+				{ intermediate: true, loose: true }
+			),
+			// Meta: s-templates.json
+			copy(
+				resolve(funcMetaPath, "s-templates.json"),
+				resolve(pathDist, funcMetaPath.slice(packageRoot.length), "s-templates.json"),
+				{ intermediate: true, loose: true }
+			),
+			// Handler and it's dependencies
+			cjsResolve(
+				"/",
+				handlerFullPath.slice(0, -func.handler.slice(func.handler.indexOf(".")).length)
+			)(function (programPath) {
+				return getDependencies(programPath).map(function (modulePath) {
+					var destPath = resolve(pathDist, modulePath.slice(packageRoot.length));
+					if (!filter(modulePath, destPath)) return null;
+					return copy(modulePath, destPath, { intermediate: true });
+				});
+			})
+		);
+	};
+};
